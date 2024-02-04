bce40047160af74cc0289fba658965fbcde58cc3
maotan <103965383+nekocarpet@users.noreply.github.com>
2024-02-04 13:04:19 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
-from distribute_setup import use_setuptools
-use_setuptools()
+# from distribute_setup import use_setuptools
+# use_setuptools()
 
 from setuptools import setup
 
